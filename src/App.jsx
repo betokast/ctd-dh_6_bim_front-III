692e8fb32b338fe29486605d0c7384e51806a7c4
robertoCastro <bkastro@gmail.com>
2022-12-11 17:28:13 -0300

    Implementação do controle de temas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 
 function App() {
+
+  const [lightTheme, setLightTheme] = useState(false)
+
+  function setarTema(){
+    setLightTheme(!lightTheme)
+  }
+
   return (
     <>
       {/* //Na linha seguinte deverá ser feito um teste se a aplicação
         // está em dark mode e deverá utilizar a classe dark ou light */}
-      <div className={`app light}`}>
+      <div className={`app ${setarTema ? 'light' : ''}`}>
         <Navbar />
         <main>
           <Outlet />
